0c85a287638e0cbe1fec48b4a3d213f155caf015
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-24 09:32:59 -0600

    Add feature test for user story 2 (avg snack price for a machine)

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -26,4 +26,16 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
       expect(page).to have_content("#{snack_2.name}: $#{snack_2.price}")
     end
   end
+
+  scenario "they see the average price of all snacks in that machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: "KitKat", price: 2.50)
+    snack_2 = dons.snacks.create(name: "Snickers", price: 1.70)
+    avg_price = (snack_1.price + snack_2.price) / 2.0
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average Price: $#{avg_price}")
+  end
 end
