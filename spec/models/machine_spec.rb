b94f42f6676d53f2359cea7f1dd650807977af9d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-24 09:40:29 -0600

    Add machine#snack_count - completes user story 2

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -20,5 +20,14 @@ RSpec.describe Machine, type: :model do
 
       expect(dons.average_price).to eq (avg_price)
     end
+
+    it "#snack_count" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_1 = dons.snacks.create(name: "KitKat", price: 2.50)
+      snack_2 = dons.snacks.create(name: "Snickers", price: 1.70)
+
+      expect(dons.snack_count).to eq (2)
+    end
   end
 end
